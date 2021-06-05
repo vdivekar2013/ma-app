7b850df4246380c36345b9cc0e516bef19ee7415
vdivekar2013 <vdivekar2013@gmail.com>
2021-06-05 15:48:16 +0100

    Date picker added as an example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="App">
       <header className="App-header">
+        <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
         <h3 className="App-logo">Hello React JS</h3>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
